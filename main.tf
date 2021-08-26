077158fec39ec899b534e426144986d4df493b18
Jorge <georgevazj@gmail.com>
2021-08-26 13:52:15 +0200

    First version

---
+++ main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    mongodbatlas = {
+      source = "mongodb/mongodbatlas"
+      version = "1.0.0"
+    }
+  }
+}
+
+# Configure the MongoDB Atlas provider
+provider "azurerm" {
+  features {}
+}
+
+provider "mongodbatlas" {
+}
+
+module "az_workload" {
+  source  = "app.terraform.io/georgevazj-lab/workload/azure"
+  version = "0.0.15"
+
+  akv_name = var.akv_name
+  description = var.description
+  resource_group_name = var.resource_group_name
+  sta_name = var.sta_name
+}
+
+module "project" {
+  source  = "app.terraform.io/georgevazj-lab/project/mongodbatlas"
+  version = "0.0.3"
+  organization_id = var.organization_id
+  project_name = var.project_name
+}
+
+module "cluster" {
+  source  = "app.terraform.io/georgevazj-lab/cluster/mongodbatlas"
+  version = "0.0.1"
+  cluster_name = var.cluster_name
+  project_id = module.project.project_id
+  mongodb_version = var.mongodb_version
+  cluster_type = var.cluster_type
+  provider_instance_size_name = var.provider_instance_size_name
+}
+
+module "dbuser" {
+  source  = "app.terraform.io/georgevazj-lab/dbuser/mongodbatlas"
+  version = "0.0.1"
+
+  database_name = var.database_name
+  database_username = var.database_username
+  database_password = var.database_password
+  project_id = module.project.project_id
+}
+
+module "ipaccess" {
+  source  = "app.terraform.io/georgevazj-lab/ipaccess/mongodbatlas"
+  version = "0.0.1"
+  project_id = module.project.project_id
+  ip_address = var.ip_access
+}
+
+module "privatelink" {
+  source  = "app.terraform.io/georgevazj-lab/privatelink/mongodbatlas"
+  version = "0.0.1"
+  azure_pve_name = var.azure_pve_name
+  project_id = module.project.project_id
+  resource_group_name = module.az_workload.resource_group_name
+  subnet_name = var.subnet_name
+  vnet_name = var.vnet_name
+}
---
+++ variables.tf
@@ -0,0 +1,101 @@
+# Azure workload
+variable "resource_group_name" {
+  type = string
+  description = "(Required) Resource group name"
+}
+
+variable "location" {
+  type = string
+  description = "(Optional) Resource group location"
+  default = "westeurope"
+}
+
+variable "description" {
+  type = string
+  description = "(Required) Resource group description"
+}
+
+# Azure Key vault
+variable "akv_name" {
+  type = string
+  description = "(Required) Key Vault name"
+}
+
+# Azure storage account
+variable "sta_name" {
+  type = string
+  description = "(Required) Storage account name"
+}
+
+# MongoDB Atlas project
+variable "organization_id" {
+  type = string
+  description = "(Required) MongoDB Atlas Organization ID"
+}
+
+variable "project_name" {
+  type = string
+  description = "(Required) MongoDB Atlas project name"
+}
+
+# MongoDB Atlas cluster
+variable "cluster_name" {
+  type = string
+  description = "(Required) MongoDB Atlas cluster name"
+}
+
+variable "mongodb_version" {
+  type = string
+  description = "(Optional) MongoDB version. Default: 4.6"
+  default = "4.2"
+}
+
+variable "cluster_type" {
+  type = string
+  description = "(Optional) MongoDB Atlas cluster type. Default: REPLICASET"
+  default = "REPLICASET"
+}
+
+variable "provider_instance_size_name" {
+  type = string
+  description = "(Required) Provider instance size name. Default: M10"
+  default = "M10"
+}
+
+# MongoDB Atlas database user
+variable "database_username" {
+  type = string
+  description = "(Required) MongoDB database username"
+}
+
+variable "database_password" {
+  type = string
+  description = "(Required) MongoDB database user password"
+}
+
+variable "database_name" {
+  type = string
+  description = "(Required) Database name"
+}
+
+variable "ip_access" {
+  type = string
+  description = "(Required) IP Address for accessing the cluster"
+  default = "0.0.0.0"
+}
+
+# Private link configuration
+variable "vnet_name" {
+  type = string
+  description = "(Required) Azure virtual network name"
+}
+
+variable "subnet_name" {
+  type = string
+  description = "(Required) Azure subnet name"
+}
+
+variable "azure_pve_name" {
+  type = string
+  description = "(Required) Azure private endpoint name"
+}
